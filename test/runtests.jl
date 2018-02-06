220180a8803855eb52beaf6596fae1e0eb19d139
roger <roger.herikstad@gmail.com>
2018-02-06 16:58:20 +0800

    Adds test for process_dirs

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,20 @@ using Base.Test
     @test _name == "20130923"
     _pth = DPHT.process_level("session", "Pancake/20130923/session01/array01/channel001")
     @test _pth == "./../.."
+    dd = tempdir()
+    cd(dd) do
+        dirs =["20140903/session01", "20140903/session02"]
+        for d2 in dirs
+            mkpath(d2)
+        end
+        func() = touch("test.txt")
+        DPHT.process_dirs(func, dirs)
+        for d2 in dirs
+            @test isfile("$(d2)/test.txt")
+        end
+        #cleanup
+        for d2 in dirs
+            rm(d2;recursive=true)
+        end
+    end
 end
