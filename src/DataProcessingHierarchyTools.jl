6b287c2e519ba56449a4da127ef8f92cdf072b0f
roger <roger.herikstad@gmail.com>
2018-02-06 13:50:19 +0800

    DataProcessingHierarchyTools.jl generated files.
        license:  MIT
        authors:  roger
        years:    2018
        user:     
    
    Julia Version 0.6.2 [d386e40c17]

---
+++ src/DataProcessingHierarchyTools.jl
@@ -0,0 +1,5 @@
+module DataProcessingHierarchyTools
+
+# package code goes here
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using DataProcessingHierarchyTools
+using Base.Test
+
+# write your own tests here
+@test 1 == 2
