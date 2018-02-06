ea7933ed5aa24210279d9e587f6deaae74992bc0
roger <roger.herikstad@gmail.com>
2018-02-06 15:15:30 +0800

    Adds test for relative path change

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,6 @@ using Base.Test
 @testset "Level functions" begin
     _name = DPHT.get_level_name("days","newWorkingMemory/Pancake/20130923/")
     @test _name == "20130923"
+    _pth = DPHT.process_level("session", "Pancake/20130923/session01/array01/channel001")
+    @test _pth == "./../.."
 end
