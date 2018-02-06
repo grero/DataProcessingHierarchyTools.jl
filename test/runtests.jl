3054c7b20a1cc0775101f2af2977abdbbdf11085
roger <roger.herikstad@gmail.com>
2018-02-06 16:21:39 +0800

    Adds another test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,8 @@ const DPHT = DataProcessingHierarchyTools
 using Base.Test
 
 @testset "Level functions" begin
+    thislevel = DPHT.level("newWorkingMemory/Pancake/20130923")
+    @test thislevel == "day"
     _name = DPHT.get_level_name("days","newWorkingMemory/Pancake/20130923/")
     @test _name == "20130923"
     _pth = DPHT.process_level("session", "Pancake/20130923/session01/array01/channel001")
