4f2b6fe84b76aa7664c73508d495c5e5079ac842
roger <roger.herikstad@gmail.com>
2018-02-06 20:14:42 +0800

    Reorganizes and adds more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,43 @@
+module DPHTTest
 using DataProcessingHierarchyTools
 const DPHT = DataProcessingHierarchyTools
 using Base.Test
+using MAT
+
+import Base.hcat
+
+struct TestData <: DPHData
+    data::String
+    setid::Vector{Int64}
+end
+
+level(::Type{TestData}) = "session"
+filename(::Type{TestData}) = "data.mat"
+
+function Base.hcat(X1::TestData, X2::TestData)
+    ndata = "$(X1.data)$(X2.data)"
+    setid = [X1.setid;X2.setid + X1.setid[end]]
+    TestData(ndata, setid)
+end
+
+
+function TestData()
+    dd = MAT.matread(filename(TestData))
+    TestData(dd["data"], dd["setid"])
+end
+
+function TestData(x)
+    X = TestData("test", [1])
+    save_data(X)
+    X
+end
+
+function save_data(X::TestData)
+    MAT.matwrite(filename(TestData), Dict("data" => X.data,
+                                          "setid" => X.setid))
+end
+
+dirs =["20140903/session01", "20140903/session02"]
 
 @testset "Level functions" begin
     thislevel = DPHT.level("newWorkingMemory/Pancake/20130923")
@@ -11,7 +48,6 @@ using Base.Test
     @test _pth == "./../.."
     dd = tempdir()
     cd(dd) do
-        dirs =["20140903/session01", "20140903/session02"]
         for d2 in dirs
             mkpath(d2)
         end
@@ -30,3 +66,25 @@ using Base.Test
         end
     end
 end
+
+@testset "Types" begin
+    dd = tempdir()
+    cd(dd) do
+        for d2 in dirs
+            mkpath(d2)
+        end
+        Y = DPHT.process_dirs(TestData, dirs, 1)
+        for d2 in dirs
+            cd(d2) do
+                @test isfile(filename(TestData))
+            end
+        end
+        @test Y.data == "testtest"
+        @test Y.setid == [1,2]
+        for d2 in dirs
+            rm(d2;recursive=true)
+        end
+    end
+end
+
+end#module
